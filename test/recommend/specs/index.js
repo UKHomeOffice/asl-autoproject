c86cff5962a22068c360d69de4a74027e858b96f
Leonard Martin <leonard.martin@gmail.com>
2019-10-01 14:04:55 +0100

    Fix tab selection in internal task lists

--- test/recommend/specs/index.js
+++ test/recommend/specs/index.js
@@ -2,6 +2,35 @@ describe('PPL Recommendation', () => {
 
   it('can recommend a PPL', () => {
     browser.withUser('inspector');
+
+    browser.click('a=In progress');
+    browser.click('a=Outstanding');
+    browser.click('a=Last changed');
+    browser.click('a=Last changed');
+
+    // find task in task list
+    assert.ok(browser.isVisible(`[title=${process.env.PROJECT_TITLE}]`));
+    console.log('Found task for project');
+    browser.$(`[title=${process.env.PROJECT_TITLE}]`).click('a=PPL application');
+
+    browser.click('a=View latest submission');
+
+    browser.click('a=Introductory details');
+
+    assert.ok(browser.isVisible(`p=${process.env.PROJECT_TITLE}`), 'Project title should be visible on introductory details review page');
+    console.log('Reviewed project');
+
+    browser.click('a=Next steps');
+
+    browser.click('input[name="status"][value="inspector-recommended"]');
+
+    browser.click('button=Continue');
+
+    browser.click('button=Recommend for approval');
+
+    assert.ok(browser.isVisible('h1=Recommendation sent'));
+    console.log('Recommended application');
+
   });
 
 });
