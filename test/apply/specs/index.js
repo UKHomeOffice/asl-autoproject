8960034194ec8770bff5747d78b4647d861f9c4e
Leonard Martin <leonard.martin@gmail.com>
2019-10-01 11:23:07 +0100

    Complete and submit PPL application

--- test/apply/specs/index.js
+++ test/apply/specs/index.js
@@ -103,8 +103,8 @@ describe('PPL Application', () => {
     completeRichTextField(browser, 'poles-justification');
     browser.$('.control-panel').click('button=Continue');
 
-    browser.$('input[name$="title"]').setValue('First POLE');
-    completeRichTextField(browser, 'pole-info');
+    browser.$('input[name$=".title"]').setValue('First POLE');
+    completeRichTextField(browser, '.pole-info');
     browser.$('.control-panel').click('button=Continue');
 
     completeRichTextField(browser, 'poles-inspection');
@@ -113,6 +113,231 @@ describe('PPL Application', () => {
     continueAndComplete(browser);
 
     assert.equal(browser.$$('.badge.complete').length, 6);
+
+    // complete scientific background
+    browser.click('a=Scientific background');
+    browser.click('input[name="scientific-background-basic-translational"][value="true"]');
+    completeRichTextField(browser, 'scientific-knowledge-summary');
+    completeRichTextField(browser, 'scientific-knowledge-details');
+    browser.click('input[name="clinical-condition"][value="false"]');
+
+    browser.click('input[name="scientific-background-producing-data"][value="false"]');
+    browser.click('input[name="scientific-background-non-regulatory"][value="false"]');
+    browser.click('input[name="scientific-background-genetically-altered"][value="false"]');
+    browser.click('input[name="scientific-background-vaccines"][value="false"]');
+    browser.click('input[name="transfer-expiring"][value="false"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 7);
+
+    // complete action plan
+    browser.click('a=Action plan');
+    browser.$('input[name$=".title"]').setValue('First objective');
+    completeRichTextField(browser, '.objective-relation');
+    browser.$('.control-panel').click('button=Continue');
+
+    completeRichTextField(browser, 'objectives-alternatives');
+    browser.click('input[name="objectives-regulatory-authorities"][value="false"]');
+    browser.click('input[name="objectives-non-regulatory"][value="false"]');
+    browser.click('input[name="objectives-genetically-altered"][value="false"]');
+    browser.click('input[name="objectives-vaccines"][value="false"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 8);
+
+    // complete general principles
+    browser.click('a=General principles');
+    completeRichTextField(browser, 'general-principles-duplicate');
+    browser.click('input[name="experimental-design-sexes"][value="true"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 9);
+
+    // complete benefits
+    browser.click('a=Benefits');
+    browser.$('.nts').click('button=Continue');
+    completeRichTextField(browser, 'benefit-outputs');
+    completeRichTextField(browser, 'benefit-who');
+    browser.click('input[name="benefit-service"][value="true"]');
+    completeRichTextField(browser, 'benefit-service-benefits');
+    completeRichTextField(browser, 'benefit-maximise-outputs');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 10);
+
+    // complete protocols
+    browser.click('a=Protocols');
+    browser.$('input[name$=".title"]').setValue('First protocol');
+    browser.$('.protocol').click('button=Continue');
+    completeRichTextField(browser, '.description');
+    browser.click('input[name$=".severity"][value="moderate"]');
+    completeRichTextField(browser, '.severity-proportion');
+    completeRichTextField(browser, '.severity-details');
+    browser.click('input[name$=".location"][value="University of Croydon"]');
+    completeRichTextField(browser, '.outputs');
+
+    browser.click('h3=Animals used in this protocol');
+    browser.click('input[name$=".species"][value="mice"]');
+    browser.click('input[name$=".life-stages"][value="adult"]');
+    browser.click('input[name$=".continued-use"][value="false"]');
+    browser.click('input[name$=".reuse"][value="false"]');
+    browser.$('input[name$=".maximum-animals"]').setValue('100');
+    browser.$('input[name$=".maximum-times-used"]').setValue('1');
+
+    browser.click('h3=Genetically altered animals (GAA)');
+    browser.click('input[name$=".gaas"][value="false"]');
+
+    browser.click('h3=Steps');
+    completeRichTextField(browser, '.title');
+    browser.click('input[name$=".nmbas"][value="false"]');
+    browser.click('input[name$=".optional"][value="false"]');
+    browser.click('input[name$=".adverse"][value="false"]');
+    browser.click('button=Save step');
+    browser.click('button=Add another step');
+    completeRichTextField(browser, '.title');
+    browser.click('input[name$=".nmbas"][value="false"]');
+    browser.click('input[name$=".optional"][value="false"]');
+    browser.click('input[name$=".adverse"][value="false"]');
+    browser.click('button=Save step');
+
+    browser.click('h3=Animal experience');
+    completeRichTextField(browser, '.experience-summary');
+    completeRichTextField(browser, '.experience-endpoints');
+
+    browser.click('h3=Experimental design');
+    browser.click('input[name$=".quantitative-data"][value="false"]');
+
+    browser.click('h3=Protocol justification');
+    completeRichTextField(browser, '.most-appropriate');
+    completeRichTextField(browser, '.most-refined');
+    completeRichTextField(browser, '.scientific-endpoints');
+    completeRichTextField(browser, '.scientific-suffering');
+    completeRichTextField(browser, '.scientific-endpoints-justification');
+    browser.click('input[name$=".justification-substances"][value="false"]');
+
+    browser.click('h3=Fate of animals');
+    browser.click('input[name$=".fate"][value="killed"]');
+
+    browser.click('input[name="complete"][value="true"]');
+    browser.$('.protocol').click('button=Continue');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 11);
+
+    // complete project harms
+    browser.click('a=Project harms');
+    browser.$('.nts').click('button=Continue');
+    completeRichTextField(browser, 'project-harms-animals');
+    completeRichTextField(browser, 'project-harms-summary');
+    completeRichTextField(browser, 'project-harms-effects');
+    completeRichTextField(browser, 'project-harms-severity');
+    completeRichTextField(browser, 'project-harms-animals');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 12);
+
+    // complete fate of animals
+    browser.click('a=Fate of animals');
+    browser.$('.nts').click('button=Continue');
+    browser.click('input[name="fate-of-animals-nts"][value="false"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 13);
+
+    // complete purpose bred animals
+    browser.click('a=Purpose bred animals');
+    browser.click('input[name="purpose-bred"][value="true"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 14);
+
+    // complete endangered animals
+    browser.click('a=Endangered animals');
+    browser.click('input[name="endangered-animals"][value="false"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 15);
+
+    // complete animals taken from the wild
+    browser.click('a=Animals taken from the wild');
+    browser.click('input[name="wild-animals"][value="false"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 16);
+
+    // complete feral animals
+    browser.click('a=Feral animals');
+    browser.click('input[name="feral-animals"][value="false"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 17);
+
+    // complete commercial slaugher
+    browser.click('a=Commercial slaughter');
+    browser.click('input[name="commercial-slaughter"][value="false"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 18);
+
+    // complete human material
+    browser.click('a=Animals containing human material');
+    browser.click('input[name="animals-containing-human-material"][value="false"]');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 19);
+
+    // complete replacement
+    browser.click('a=Replacement');
+    browser.$('.nts').click('button=Continue');
+    completeRichTextField(browser, 'replacement-why');
+    completeRichTextField(browser, 'replacement-alternatives');
+    completeRichTextField(browser, 'replacement-justification');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 20);
+
+    // complete reduction
+    browser.click('a=Reduction');
+    browser.$('.nts').click('button=Continue');
+    browser.$('input[name="reduction-quantities-mice"]').setValue('100');
+    browser.$('input[name="reduction-quantities-rats"]').setValue('100');
+    completeRichTextField(browser, 'reduction-estimation');
+    completeRichTextField(browser, 'reduction-steps');
+    completeRichTextField(browser, 'reduction-review');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 21);
+
+    // complete refinement
+    browser.click('a=Refinement');
+    browser.$('.nts').click('button=Continue');
+    completeRichTextField(browser, 'refinement-models');
+    completeRichTextField(browser, 'refinement-less-sentient');
+    completeRichTextField(browser, 'refinement-3rs-advances');
+    completeRichTextField(browser, 'refinement-explaination');
+    completeRichTextField(browser, 'refinement-published-guidance');
+
+    continueAndComplete(browser);
+    assert.equal(browser.$$('.badge.complete').length, 22);
+
+    // complete nts review
+    browser.click('a=Review');
+    browser.click('input[name="complete"][value="true"]');
+    browser.click('button=Continue');
+
+    assert.equal(browser.$$('.badge.complete').length, 23);
+
+    // submit application
+    browser.click('button=Continue');
+    browser.click('input[name="authority"][value="Yes"]');
+    browser.$('input[name="authority-pelholder-name"]').setValue('Bruce Banner');
+    browser.$('input[name="authority-endorsement-date"]').setValue('1/3/2019');
+    browser.click('input[name="awerb"][value="Yes"]');
+    browser.$('textarea[name="awerb-review-date"]').setValue('University of Croydon - 2/3/2019');
+    browser.click('input[name="ready"][value="Yes"]');
+    browser.click('button=Submit PPL application');
+
+    assert.ok(browser.isVisible('h1=Application sent'));
 
   });
 
