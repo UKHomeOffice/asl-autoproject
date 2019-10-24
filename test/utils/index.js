38fdfe73b1f57544f0225a1027f8eb14bd59f604
Mark Stringer <mark.stringer@mumbly.co.uk>
2019-10-24 06:56:28 +0100

    Update index.js

--- test/utils/index.js
+++ test/utils/index.js
@@ -18,16 +18,16 @@ const sentence = (min = 10, max = 60, newline = true) => {
 };
 
 
-const paragraphs = (min = 1, max = 3, fast=false) => {
+const paragraphs = (min = 1, max = 3, fast = false) => {
   const result = [];
-  const min_words = fast?1:10;
-  const max_words = fast?10:100;
+  const minWords = fast ? 1 : 10;
+  const maxWords = fast ? 10 : 100;
   const len = between(min, max);
   while (result.length < len) {
-    result.push(sentence(min_words,max_words));
+    result.push(sentence(minWords, maxWords));
   }
   return result;
-};
+}
 
 module.exports = {
   words,
