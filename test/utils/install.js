d6bb86d588a2fa889986047ef716f5f30f423aa3
Leonard Martin <leonard.martin@gmail.com>
2020-06-02 16:11:10 +0100

    Set implicit wait element timeout to 5 seconds
    This is zero by default, which might be leading to some flakiness.

--- test/utils/install.js
+++ test/utils/install.js
@@ -2,6 +2,7 @@ const gotoOutstandingTasks = require('./goto-outstanding-tasks');
 const completeRichText = require('./complete-rich-text');
 
 module.exports = () => {
+  browser.setTimeout({ implicit: 5000 });
   browser.addCommand('gotoOutstandingTasks', gotoOutstandingTasks);
   browser.addCommand('completeRichText', completeRichText, true);
 };
