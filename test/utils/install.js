5665360829d69d680f9af75eb21ae50cb0d035e1
Joe Fitter <joefitter@hotmail.com>
2020-06-03 14:33:28 +0100

    Update test/utils/install.js
    Co-authored-by: Leonard Martin <leonard.martin@gmail.com>

--- test/utils/install.js
+++ test/utils/install.js
@@ -3,6 +3,7 @@ const completeRichText = require('./complete-rich-text');
 const gotoPlaces = require('./goto-places');
 
 module.exports = () => {
+  browser.setTimeout({ implicit: 200 });
   browser.addCommand('gotoOutstandingTasks', gotoTasks('Outstanding'));
   browser.addCommand('gotoCompletedTasks', gotoTasks('Completed'));
   browser.addCommand('completeRichText', completeRichText, true);
